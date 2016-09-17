64737c3c26024add5a535d56397da5de5a17be07
phonofidelic <chris_clemons85@hotmail.com>
2016-09-17 15:27:32 -0400

    fix: fetch serviceworker now serves cached resources

--- src/sw.js
+++ src/sw.js
@@ -4,12 +4,12 @@ self.addEventListener('install', function(event) {
 	event.waitUntil(
 		caches.open(staticCacheName).then(function(cache) {
 			return cache.addAll([
-				// 'index.html',
-				// 'css/main.min.css',
-				// 'app/app.min.js',
-				// 'lib/deps.min.js',
-				// 'css/deps.min.css',
-				// 'app/templates/planTrip.html'
+				'/',
+				'css/main.min.css',
+				'app/app.min.js',
+				'lib/deps.min.js',
+				'css/deps.min.css',
+				'app/templates/planTrip.html'
 			]);
 		})
 	);
@@ -31,39 +31,18 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-	// if (event.request.url.indexOf('.zip') != -1) {
-	// 	console.log('zip request detected...')
-
-	// 	var requestUrl = new URL(event.request.url);
-
-	// 	event.respondWith(
-	// 		caches.match(event.request).then(function(response) {
-	// 			if (response) {
-	// 				console.log('Found response in cache: ', response);
-	// 				return response;
-	// 			}
-	// 			console.log('No response found in cache. About to fetch from network...');
-	// 			var init = { 
-	// 				headers: {
-	// 					'Content-Type': 
-	// 				}, 
-	// 				mode: 'coors'}
-	// 			return fetch(requestUrl, init).then(function(response) {
-	// 				console.log('Response from network: ', response);
-	// 				return response;
-	// 			}).catch(function(err) {
-	// 				console.error('Fetch from network failed: ', err);
-	// 				throw err;
-	// 			});
-	// 		})
-	// 	);
-	// }
-
 	if (event.request.url.indexOf('https://maps.googleapis.com') == -1 && event.request.url != 'http://www.broward.org/bct/google/latest/google_transit.zip') {
 		console.log('fetch event for: ', event.request.url)
 
 		var requestUrl = new URL(event.request.url);
 		console.log('requestUrl: ', requestUrl);
+
+		if (requestUrl.origin === location.origin) {
+			if (requestUrl.pathname === '/') {
+				event.respondWith(caches.match('/'));
+				return;
+			}
+		}
 
 		event.respondWith(
 			caches.match(event.request).then(function(response) {
